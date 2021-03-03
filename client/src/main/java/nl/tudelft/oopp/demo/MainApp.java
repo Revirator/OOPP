5183cc651c3f82a3aa37c2a2fe8d6f5d98ab6f69
Senne Van den Broeck <S.M.Z.VandenBroeck@student.tudelft.nl>
2021-03-03 20:36:35 +0100

    Progress of StudentView

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -2,9 +2,10 @@ package nl.tudelft.oopp.demo;
 
 import nl.tudelft.oopp.demo.views.QuoteDisplay;
 import nl.tudelft.oopp.demo.views.SplashView;
+import nl.tudelft.oopp.demo.views.StudentView;
 
 public class MainApp {
     public static void main(String[] args) {
-        SplashView.main(new String[0]);
+        StudentView.main(new String[0]);
     }
 }
