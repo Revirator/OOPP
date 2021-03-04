59b689a933e033a45bedba51fa8ff5730ee13b50
Senne Van den Broeck <S.M.Z.VandenBroeck@student.tudelft.nl>
2021-03-04 23:01:15 +0100

    Finished moderator view

--- client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
+++ client/src/main/java/nl/tudelft/oopp/demo/MainApp.java
@@ -1,11 +1,9 @@
 package nl.tudelft.oopp.demo;
 
-import nl.tudelft.oopp.demo.views.QuoteDisplay;
 import nl.tudelft.oopp.demo.views.SplashView;
-import nl.tudelft.oopp.demo.views.StudentView;
 
 public class MainApp {
     public static void main(String[] args) {
-        StudentView.main(new String[0]);
+        SplashView.main(new String[0]);
     }
 }
--- client/src/main/java/nl/tudelft/oopp/demo/views/ModeratorView.java
+++ client/src/main/java/nl/tudelft/oopp/demo/views/ModeratorView.java
@@ -8,24 +8,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 
-public class StudentView extends Application {
+public class ModeratorView extends Application {
 
     /**
-     * Font sizes for student screen.
+     * Font sizes for moderator screen.
      */
     private DoubleProperty subTitleFontSize = new SimpleDoubleProperty(10);
     private DoubleProperty tabFontSize = new SimpleDoubleProperty(10);
-    private DoubleProperty pollButtonFontSize = new SimpleDoubleProperty(10);
+    private DoubleProperty percentageFontSize = new SimpleDoubleProperty(10);
     private DoubleProperty buttonFontSize = new SimpleDoubleProperty(10);
     private DoubleProperty textBoxFontSize = new SimpleDoubleProperty(10);
+    private DoubleProperty normalFontSize = new SimpleDoubleProperty(10);
 
     /**
-     * Creates the student screen scene and loads it on the primary stage.
+     * Creates the moderator screen scene and loads it on the primary stage.
      * @param primaryStage primary stage of the app
      * @throws IOException if FXMLLoader fails to load the url
      */
@@ -34,7 +36,7 @@ public class StudentView extends Application {
 
         // Load file
         FXMLLoader loader = new FXMLLoader();
-        URL xmlUrl = getClass().getResource("/studentRoom.fxml");
+        URL xmlUrl = getClass().getResource("/moderatorRoom.fxml");
         loader.setLocation(xmlUrl);
         Parent root = loader.load();
 
@@ -45,6 +47,16 @@ public class StudentView extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+        // Add options to dropdown buttons
+        ChoiceBox<String> answers = (ChoiceBox) root.lookup("#answerAmount");
+        ChoiceBox<String> correctAnswer = (ChoiceBox) root.lookup("#correctAnswer");
+
+        for (int i=1; i<11; i++)
+            answers.getItems().add(String.valueOf(i));
+
+        for(char letter = 'A'; letter <='J'; letter++ )
+            correctAnswer.getItems().add(String.valueOf(letter));
+
         // Bind font sizes to screen size
         subTitleFontSize.bind(scene.widthProperty().add(scene.heightProperty()).divide(85));
 
@@ -52,14 +64,17 @@ public class StudentView extends Application {
         tabFontSize.bind(Bindings.min(15,
                 scene.widthProperty().add(scene.heightProperty()).divide(85)));
 
-        pollButtonFontSize.bind(Bindings.min(15,
-                scene.widthProperty().add(scene.heightProperty()).divide(100)));
+        percentageFontSize.bind(Bindings.min(45,
+                scene.widthProperty().add(scene.heightProperty()).divide(45)));
 
         buttonFontSize.bind(Bindings.min(15,
                 scene.widthProperty().add(scene.heightProperty()).divide(120)));
 
-        textBoxFontSize.bind(Bindings.min(25,
-                scene.widthProperty().add(scene.heightProperty()).divide(75)));
+        textBoxFontSize.bind(Bindings.min(18,
+                scene.widthProperty().add(scene.heightProperty()).divide(100)));
+
+        normalFontSize.bind(Bindings.min(18,
+                scene.widthProperty().add(scene.heightProperty()).divide(100)));
 
         // Put the font sizes on all according nodes
         for (Node node : root.lookupAll(".subTitleText")) {
@@ -73,9 +88,9 @@ public class StudentView extends Application {
         }
 
 
-        for (Node node : root.lookupAll(".pollButton")) {
+        for (Node node : root.lookupAll(".normalText")) {
             node.styleProperty().bind(Bindings.concat("-fx-font-size: ",
-                    pollButtonFontSize.asString(), ";"));
+                    normalFontSize.asString(), ";"));
         }
 
         for (Node node : root.lookupAll(".buttonText")) {
@@ -88,10 +103,14 @@ public class StudentView extends Application {
                     textBoxFontSize.asString(), ";"));
         }
 
+        for (Node node : root.lookupAll(".percentage")) {
+            node.styleProperty().bind(Bindings.concat("-fx-font-size: ",
+                    percentageFontSize.asString(), ";"));
+        }
+
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
