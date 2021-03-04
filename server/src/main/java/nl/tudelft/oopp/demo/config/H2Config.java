135678758c7264a6e8b720ee57e714bad78966a1
nkuo <h.n.kuo@student.tudelft.nl>
2021-03-04 12:01:17 +0100

    added H2 to build.gradle dependencies again

--- server/src/main/java/nl/tudelft/oopp/demo/config/H2Config.java
+++ server/src/main/java/nl/tudelft/oopp/demo/config/H2Config.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@Profile("development")
+@Profile("development")
 @Configuration
 @EnableJpaRepositories("nl.tudelft.oopp.demo")
 @PropertySource("application-dev.properties")
