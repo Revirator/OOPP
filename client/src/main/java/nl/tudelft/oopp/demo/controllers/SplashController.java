b0c1cc3d698d589c6f16bf59ca283e6117db19d0
Pavel Germanov <p.germanov@student.tudelft.nl>
2021-03-04 11:53:27 +0100

    create base of SplashController connect SplashController to splash.fxml

---
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/SplashController.java
@@ -0,0 +1,19 @@
+package nl.tudelft.oopp.demo.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SplashController {
+
+    @FXML
+    private TextField nickName;     // the value of the nickname text box
+
+    @FXML
+    private TextField link;     // the value of the link text box
+
+    public void buttonClicked(ActionEvent actionEvent) {
+
+        
+    }
+}
