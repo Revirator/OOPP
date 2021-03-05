1fd76c020a9082d49b61e2e2085e116e23d4e70f
Pavel Germanov <p.germanov@student.tudelft.nl>
2021-03-05 16:50:41 +0100

    add id to anchorPane of splash.fxml, change of view works when a valid code is entered

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/SplashController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/SplashController.java
@@ -3,14 +3,18 @@ package nl.tudelft.oopp.demo.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.data.Room;
 
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDateTime;
 
 public class SplashController {
@@ -21,10 +25,13 @@ public class SplashController {
     @FXML
     private TextField link;     // the value of the link text box
 
+    @FXML
+    private AnchorPane anchor;      // the splash.fxml anchor pane
+
     /**
      * Handles clicking the button.
      */
-    public void buttonClicked(ActionEvent actionEvent) {
+    public void buttonClicked(ActionEvent actionEvent) throws IOException {
         if(nickName.getText().equals("") || link.getText().equals("")) {    // Check if one of the fields is empty
 
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -39,17 +46,30 @@ public class SplashController {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
 
             if(room == null) {
-                alert.setContentText("Invalid room link.");
+                alert.setContentText("Invalid room link.");     // The room is null when the code is invalid
                 link.clear();
             } else {
                 String role;
                 if(isStudent(link.getText())) role = "student";
                 else role = "moderator";
 
-                if(room.getStartingTime().isBefore(LocalDateTime.now())) {   // Here there has to be proper check for if the room is currently open but
+                if(room.getStartingTime().isBefore(LocalDateTime.now())) {   // Might need improvement but works for now
                     alert.setContentText("You joined " + room.getRoomName() + " as a " + role); // Here the view should change to the room view
+
+                    // The next few lines are to change the view to the room view
+                    // Most of it is magic to me ngl, but it works
+                    FXMLLoader loader = new FXMLLoader();
+                    URL xmlUrl = getClass().getResource("/mainScene.fxml"); // mainScene.fxml is just a placeholder for the actual scene
+                    loader.setLocation(xmlUrl);
+                    Parent root = (Parent) loader.load();
+
+                    Stage stage = (Stage) anchor.getScene().getWindow();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+
                 } else {
-                    alert.setContentText("The room is not open yet."); // Here the view should change to the waiting room view
+                    alert.setContentText("The room is not open yet."); // Here the view should change to the waiting room view instead
                 }
             }
             alert.show();
