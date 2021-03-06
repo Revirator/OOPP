1af38573eaae3f16528544ee1e64d271fce30e26
Pavel Germanov <p.germanov@student.tudelft.nl>
2021-03-06 16:46:12 +0100

    add javadoc to setData methods

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/ModeratorRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/ModeratorRoomController.java
@@ -7,6 +7,11 @@ public class ModeratorRoomController {
     private String name;
     private Room room;
 
+    /**
+     * Used in SplashController to pass the username and the room object.
+     * @param name the name entered by the user in splash
+     * @param room the room corresponding to the code entered
+     */
     public void setData(String name, Room room) {
         this.name = name;
         this.room = room;
