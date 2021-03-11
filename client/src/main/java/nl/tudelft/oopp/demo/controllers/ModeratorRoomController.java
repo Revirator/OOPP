101dd7413fc90085c7545d9d19719781f4c53250
Denis Tsvetkov <d.r.tsvetkov@student.tudelft.nl>
2021-03-11 14:23:33 +0200

    Some initial additions

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/ModeratorRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/ModeratorRoomController.java
@@ -1,8 +1,15 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import nl.tudelft.oopp.demo.data.Room;
 
 public class ModeratorRoomController {
+
+    @FXML
+    private AnchorPane anchor = new AnchorPane();
 
     private String name;
     private Room room;
@@ -15,5 +22,18 @@ public class ModeratorRoomController {
     public void setData(String name, Room room) {
         this.name = name;
         this.room = room;
+    }
+
+    public void endLecture() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText("Are you sure you want to end the lecture?");
+        alert.showAndWait();
+        if(alert.getResult().getText().equals("OK")) {
+            room.hasEnded();
+            // maybe close the window only for the students
+            // and show an alert to the moderators that the lecture has ended successfully
+            Stage stage = (Stage) anchor.getScene().getWindow();
+            stage.close();
+        }
     }
 }
--- client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentRoomController.java
@@ -1,8 +1,18 @@
 package nl.tudelft.oopp.demo.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import nl.tudelft.oopp.demo.data.Room;
 
 public class StudentRoomController {
+
+    @FXML
+    private Button submit;
+
+    @FXML
+    private TextArea question;
 
     private String name;
     private Room room;
@@ -15,5 +25,24 @@ public class StudentRoomController {
     public void setData(String name, Room room) {
         this.name = name;
         this.room = room;
+    }
+
+    public void submitQuestion() {
+        if(this.room.isActive()) {
+            if(question.textProperty().equals("Type your question here...")) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("You need to enter a question to submit it!");
+                alert.show();
+            }
+        }
+        else {
+            // It can be either this code or disabling the button
+            // Personally I like this more
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("The lecture is over. You cannot ask questions anymore!");
+            alert.show();
+            question.setDisable(true);
+            submit.setDisable(true);
+        }
     }
 }
