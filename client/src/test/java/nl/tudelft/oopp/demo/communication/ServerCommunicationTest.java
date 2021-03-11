123233a2034ab9cbc877f55b09f93c8acf4ee7be
Emke de Groot <E.A.deGroot-1@student.tudelft.nl>
2021-03-11 20:10:37 +0100

    Deleted the test because checkstyle doesn't like the commented out test

--- client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
+++ client/src/test/java/nl/tudelft/oopp/demo/communication/ServerCommunicationTest.java
@@ -21,16 +21,4 @@ public class ServerCommunicationTest {
         assertNull(ServerCommunication.makeRoom(null));
     }
 
-    /**
-     * Doesn't work in pipeline Gitlab, but does work here.
-     * Works when you start "Demoapplication" first.
-    @Test
-    public void testMakeRoom() {
-        Room room = new Room("CSE1105", LocalDateTime.now(), true);
-        Room updatedRoom = ServerCommunication.makeRoom(room);
-        assertNotNull(updatedRoom.getModeratorLink());
-        assertNotNull(updatedRoom.getStudentsLink());
-    }
-    */
-
 }
