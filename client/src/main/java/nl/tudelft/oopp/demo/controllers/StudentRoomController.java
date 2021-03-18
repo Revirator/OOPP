1873eb13a439290f8e4f25bda8c8cd96e2b35817
Emke de Groot <E.A.deGroot-1@student.tudelft.nl>
2021-03-19 00:07:46 +0100

    User has to wait for 20 seconds after a question is enterered

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentRoomController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/StudentRoomController.java
@@ -3,6 +3,8 @@ package nl.tudelft.oopp.demo.controllers;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -10,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 import nl.tudelft.oopp.demo.communication.ServerCommunication;
 import nl.tudelft.oopp.demo.data.Question;
 import nl.tudelft.oopp.demo.data.Room;
@@ -42,6 +45,8 @@ public class StudentRoomController {
     private Room room;
     private StudentView studentView;
 
+    private boolean questionAllowed;
+
     /** Used in SplashController to pass the user and the room object.
      * Data injected by start() in StudentView.
      * @param student the student that is using the window
@@ -53,6 +58,26 @@ public class StudentRoomController {
         this.room = room;
         this.studentView = studentView;
         this.lectureName.setText(this.room.getRoomName());
+        this.questionAllowed = true;
+
+        // creates a service that allows a method to be called every timeframe
+        ScheduledService<Boolean> serviceAllow = new ScheduledService<>() {
+            @Override
+            protected Task<Boolean> createTask() {
+                return new Task<>() {
+                    @Override
+                    protected  Boolean call() {
+                        updateMessage("Checking for updates..");
+                        return true;
+                    }
+                };
+            }
+        };
+
+        // every 20 seconds, questionAllowed is set on true again
+        serviceAllow.setPeriod(Duration.seconds(20));
+        serviceAllow.setOnRunning(e -> questionAllowed=true);
+        serviceAllow.start();
 
         // Next 3 lines are to execute the question refreshing every X seconds
         Timer t = new Timer();
@@ -85,6 +110,10 @@ public class StudentRoomController {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setContentText("Please enter at least 8 characters!");
                 alert.show();
+            } else if (!questionAllowed) {
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setContentText("Please wait for a total of 20 seconds before\nsubmitting another question");
+                alert.show();
             } else {
                 // Create new question, id returned by server (needed for delete/edit).
                 Question newQuestion = new Question(this.room.getRoomId(), questionBox.getText(),
@@ -94,6 +123,8 @@ public class StudentRoomController {
 
                 questionBox.clear();
                 this.studentView.addQuestion(newQuestion);
+
+                questionAllowed = false;
             }
         } else {
             Alert alert = new Alert(Alert.AlertType.WARNING);
