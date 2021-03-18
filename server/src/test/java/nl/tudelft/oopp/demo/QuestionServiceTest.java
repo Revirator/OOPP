67df878bc2182f37cbe06b2f8592b810adbddee0
Emke de Groot <E.A.deGroot-1@student.tudelft.nl>
2021-03-18 21:55:41 +0100

    Small mistake fixed with imports

--- server/src/test/java/nl/tudelft/oopp/demo/QuestionServiceTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/QuestionServiceTest.java
@@ -1,6 +1,8 @@
 package nl.tudelft.oopp.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
