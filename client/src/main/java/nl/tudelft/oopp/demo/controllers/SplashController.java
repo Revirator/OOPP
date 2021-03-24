3e0ce9115947b179c9886d3ef74dc7b132be7a45
Pavel Germanov <p.germanov@student.tudelft.nl>
2021-03-24 19:42:52 +0100

    Checkstyle fix (again)

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/SplashController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/SplashController.java
@@ -208,8 +208,8 @@ public class SplashController {
             alert.setContentText("The name and the link cannot contain empty spaces.");
             flag = false;
 
-        } else if (name.contains("/") || code.contains("/") ||
-                name.contains("=") || code.contains("=")) {
+        } else if (name.contains("/") || code.contains("/")
+                || name.contains("=") || code.contains("=")) {
             alert.setContentText("The name or the link contains illegal characters.");
             flag = false;
 
